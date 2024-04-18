97eccfb16662cf300688b18bab57345f478b2db4
Darren Deng <darrent211@gmail.com>
2024-04-18 10:59:27 -0400

    a minor update on component

--- src/components/CopyRight.tsx
+++ src/components/CopyRight.tsx
@@ -6,7 +6,7 @@ export default function Copyright() {
     return (
         <Typography variant="body2" color="text.secondary" align="center">
             {'Copyright © '}
-            <MuiLink color="inherit" href="https://github.com/kguniverse">
+            <MuiLink color="inherit" href="https://git.uwaterloo.ca/s3deng/ece656_project_group3.git">
                 ECE656 Capstone Project
             </MuiLink>{' '}
             {new Date().getFullYear()}
